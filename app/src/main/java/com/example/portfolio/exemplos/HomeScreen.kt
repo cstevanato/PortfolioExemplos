e83e8ec6742ec8cf59b258be3b95afe6bb0b68b6
Claudio Stevanato <claudio.stevanato-ext@ab-Inbev.com>
2025-12-12 10:31:58 -0300

    fix

--- app/src/main/java/com/example/portfolio/exemplos/HomeScreen.kt
+++ app/src/main/java/com/example/portfolio/exemplos/HomeScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.portfolio.exemplos
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
@@ -5,11 +7,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.portfolio.exemplos.components.CardMenu
 import com.example.portfolio.exemplos.components.CardModel
 import com.example.portfolio.exemplos.model.ProjectModel
@@ -27,7 +33,12 @@ fun HomeScreen(
     Scaffold(
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Exemplos")
+            })
+        }
     ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
