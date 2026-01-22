7535fbc4c852a6ddac43ce1281b3eeeb0b07bbf5
Claudio Stevanato <claudio.stevanato@ciandt.com>
2026-01-22 16:12:17 -0300

    WIP

--- app/src/main/java/com/example/portfolio/exemplos/features/bottomsheets/BottomSheetsScreen.kt
+++ app/src/main/java/com/example/portfolio/exemplos/features/bottomsheets/BottomSheetsScreen.kt
@@ -1,7 +1,9 @@
 package com.example.portfolio.exemplos.features.bottomsheets
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomSheetScaffold
@@ -23,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.portfolio.exemplos.R
 import kotlinx.coroutines.launch
 
